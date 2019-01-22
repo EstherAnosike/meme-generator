e88f10b4e19557bda9c007ccfe6a1b64228d84b3
Esther Anosike <estheranosike@outlook.com>
2019-01-22 11:31:35 +0000

    add all meme-generator component

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+    return ( 
+        <div className="App">
+            <header>
+                <img 
+                    src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+                    alt="Troll-face" 
+                />
+                <p>
+                    Meme Genetrator.
+                </p>
+            </header>
+        </div>
+    );
+}
+ 
+export default Header;
